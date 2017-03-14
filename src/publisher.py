c1875dd448214dc870729d16fc5f06a095a43b5b
siminwen <15191887858@qq.com>
2017-03-14 10:27:35 -0500

    all done

--- src/publisher.py
+++ src/publisher.py
@@ -27,7 +27,7 @@ zipcode = int(sys.argv[2]) if len(sys.argv) > 1 else 37215
 print("send zipcode %i" % zipcode)
 
 ShutDownTime = 0
-while ShutDownTime < 10:
+while ShutDownTime < 1000:
 
     temperature = randrange(-80, 135)
     relhumidity = randrange(10, 60)
